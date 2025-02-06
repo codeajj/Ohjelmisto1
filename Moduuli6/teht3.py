eb7208b8c1c2cabf84935df3557a017b7559aced
codeajj <paavobro@gmail.com>
2025-02-06 13:28:30 +0200

    Korjaus

--- Moduuli6/teht3.py
+++ Moduuli6/teht3.py
@@ -1,15 +1,11 @@
-def lasku(määrä):
-    gallon = 3.785
-    litrat = määrä * gallon
+def lasku(para):
+    litrat = para * 3.785
     return litrat
 
-gallonat = []
-
 while True:
-    gallon = int(input("Anna minulle gallonamäärä ja lopusi muutan ne litroiksi, negatiivinen luku lopettaa: "))
-    if gallon >= 0:
-        gallonat.append(gallon)
+    luku = int(input("Anna minulle gallonamäärä ja lopusi muutan ne litroiksi, negatiivinen luku lopettaa: "))
+    if luku >= 0:
+        tulos = lasku(luku)
+        print(f"Antamasi gallonamäärä on {tulos:.3f} litraa")
     else:
         break
-määrä = sum(gallonat)
-print(f"Antamasi gallonamäärä on {lasku(määrä):.3f} litraa")
